e12d12b9dae869cb47e88283e97db7b16205f44b
john-mcgowan1992 <john-mcgowan1992@users.noreply.github.com>
2015-10-16 23:16:31 -0400

    Update guessingGame.js

--- guessingGame.js
+++ guessingGame.js
@@ -3,29 +3,28 @@ var targetNum = numGenerate();
 var guessLog = [];
 var guessCounter = 0;
 var targetDistance;
-var currentGuess = 0; 
+var currentGuess; 
 
 
-//generate targer number 
+//generate random target number 
 function numGenerate() {
 	return Math.floor(Math.random() * 100) + 1;
 }
 
 //set currentGuess with input
 function setGuess() {
-	$("#guessButton").click(function() {
-	currentGuess = +$("#guessInput").val();
-	guessLog.push(currentGuess);
-	//define distance
+	currentGuess = Number($("#guessInput").val());
+		//define distance
 	targetDistance = currentGuess - targetNum;
-	});
-	//increment counter & store in guessLog
+		//increment counter & store in guessLog
 	guessCounter ++;
-	alert(runGame());
+	guessLog.push(currentGuess);
+	$("#guessInput").val("");
+	validRun();
 }
 
 
-//define distance from target
+//define heat signal
 function guessHelp() {
 	if (targetDistance > 40 || targetDistance < -40) {
 		return "very cold. "
@@ -43,6 +42,8 @@ function guessHelp() {
 		return "scalding hot!";
 	}
 	else {
+			//reset background when user enters correct number 
+		$('body').css('background-image', 'url(https://idisciple.blob.core.windows.net/idm/Let-the-Glory-of-God-Dwell-Upon-You.png)')
 		return "right on the money!";
 	}
 }
@@ -56,35 +57,85 @@ function guessGuidance() {
 	else if (targetDistance > 0) {
 		return "You are " + guessHelp() + " You need to guess lower!";
 	}
-	else if (targetDistance === 0) 
+	else 
 	{
 		return "You are " + guessHelp() + " You win!"
 	}
 }
 
-//core game function
-function runGame() {
-if (guessCounter > 5) {
-	return "I'm sorry; you've used up all of your guesses. Please press 'Try Again' to restart the game.";
+//append guessGuidance text below input box and clear any previous strings
+function appender(str, id) {
+	$(id).empty();
+	$(id).text(str);
 }
-else if (guessLog.indexOf(currentGuess) !== -1) {
-	return "You already tried that number! Guess again!";
-}
-else {
-	return guessGuidance();
+
+// check number repeat function 
+ //couldn't seem to get this function to properly run... every time i added it to the conditionals of validRun() it would return true
+function didRepeat() {
+	if (guessLog.indexOf(currentGuess) !== -1) {
+		return true;
 	}
 }
 
-function newGame() {
 
+//run game with validations
+function validRun() {
+if(isNaN(currentGuess)) {
+		return appender("Hey that's not a number! You just lost a guess.", '#textBox');
+	}
+else if (currentGuess < 1 || currentGuess > 100) {
+	return appender("Well that was a waste of a guess! 1-100 please", '#textBox');
+}
+// couldn't get this to run as needed 
+/* if (didRepeat()) {
+	return "You have already tried that number. Guess again!"
+}*/
+else if (guessCounter > 5) {
+	return appender("You're outta guesses! Play again at your own risk.", '#textBox');
+}
+else  {
+	return appender(guessGuidance(), '#textBox');
+	}
 }
 
-/*this additional code is just to see that the logic is working 
-currentGuess= 10;
-guessLog[0] = 30;
-targetDistance = currentGuess - targetNum;
-console.log(runGame(), "\n" + "Guess: " + currentGuess, "\n" + "Target: " + targetNum);
-*/
+//newGame function for 'new game Button'
+function newGame() {
+	targetNum = Math.floor(Math.random() *101);
+	guessLog = [];
+	guessCounter = 0;
+	targetDistance= 0;
+	$('#textBox').empty();
+	$('body').css('background-image', 'url(https://lh3.googleusercontent.com/-4PTjS9NbloE/VVJvHnm_txI/AAAAAAAAAbM/xBp4Qkhxrpc/w1452-h968-no/IMG_2184.jpg');
+}
+
+//redo button functionality
+$('#Redobutton').click(function() {
+	newGame();
+});
+
+//hint button functionality
+$('#showAnswer').click(function() {
+	return appender("The Answer is: " + targetNum, '#textBox');
+});
+
+// i played around with a number of different ways but couldnt figure out how to appen a list or a button that showed a list
+	//the following code was my attempt to add a guess log to the page; i failed to do so
+/*show list of guesses 
+var listStr = "Last Guess: " + guessLog[guessCounter-1] + "\n" + "It was " + guessHelp();
+		appender(listStr, "#guessList");
+	*/	
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 
